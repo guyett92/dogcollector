0dc49d79bbc1dfbf1a4c140c28ca15448b0ccc06
guyett92 <aaronguyett@yahoo.com>
2020-09-12 12:55:42 -0700

    updates settings again

--- dogcollector/settings.py
+++ dogcollector/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -126,4 +125,9 @@ LOGIN_REDIRECT_URL = '/dogs/'
 
 LOGOUT_REDIRECT_URL = '/'
 
+
+# Production Settings - Uncomment before heroku deployment
+
+import django_heroku
 django_heroku.settings(locals())
+
